d0da92397e79a2e5d988aff3dbca328ae86dbf91
adrarom <adrarom@gmail.com>
2025-09-17 22:13:12 +0200

    feat: added bike entity

--- src/modules/bikes/entities/bike.entity.ts
+++ src/modules/bikes/entities/bike.entity.ts
@@ -1,1 +1,45 @@
-export class Bike {}
+
+import { Entity, Column, PrimaryGeneratedColumn, CreateDateColumn, UpdateDateColumn } from 'typeorm';
+
+@Entity()
+export class Bike {
+  @PrimaryGeneratedColumn('uuid')
+  id: number;
+
+  @Column({ type: 'varchar', nullable: false})
+  userId: string;
+
+  @Column({ type: 'varchar', nullable: false})
+  brand: string;
+
+  @Column({ type: 'varchar', nullable: false})
+  model: string;
+
+  @Column({ type: 'int', nullable: false})
+  year: number;
+
+  @Column({ type: 'varchar', nullable: false})
+  plateNumber: string;
+
+  @Column({ type: 'varchar', nullable: false})
+  bikeType: string;
+
+  @Column({ type: 'varchar', nullable: false})
+  vinNumber: string;
+
+  @Column({ type: 'int', nullable: false})
+  mileage: number;
+
+  @Column({ type: 'timestamp', nullable: true })
+  lastRevision: Date;
+
+  @Column({ default : 'active', type: 'varchar', nullable: false})
+  status: string;
+
+  @CreateDateColumn({ type: 'timestamp'})
+  createdAt: Date;
+
+  @UpdateDateColumn({ type: 'timestamp'})
+  updatedAt: Date;
+
+}
